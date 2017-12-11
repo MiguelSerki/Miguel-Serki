44e1739ae31d8efefbc3a9cfe3f54d9745292da1
MiguelSerki <miguel.serki@gmail.com>
2017-12-11 13:54:42 -0300

    Singleton

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton a = Singleton.Instance();
+            Singleton b = Singleton.Instance();
+
+            if (a == b)
+            {
+                Console.WriteLine("Los objetos son los mismos");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Singleton/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Singleton
+    {
+        public static Singleton instance;
+        protected Singleton()
+        {
+
+        }
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
